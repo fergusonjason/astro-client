a46c828cbb6960ead46bcfc04ab5983f8dae1914
Jason Ferguson <fergusonjason@gmail.com>
2019-10-27 06:29:18 -0400

    add the YaleComponent. Remove the Material modules, they won't be used in this project

--- typescript-with-angular-basic/src/app/app.module.ts
+++ typescript-with-angular-basic/src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HdComponent } from './hd/hd.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HdComponent } from "./hd/hd.component";
 
-import { AstroService } from './astro.service';
-import { YaleComponent } from './yale/yale.component';
-import { MatTableModule, MatInputModule } from "@angular/material";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AstroService } from "./astro.service";
+import { YaleComponent } from "./yale/yale.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     HdComponent,
-    YaleComponent
+    YaleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    MatInputModule,
-    MatTableModule,
     BrowserAnimationsModule
   ],
   providers: [AstroService],
