75975d642c28621337fe284b9bfb6b68cc33c194
Jason Ferguson <fergusonjason@gmail.com>
2019-11-11 06:27:12 -0500

    fix quotemark, add Forms/ReactiveForms/Material/HttpClient modules, add AstroService as provider

--- typescript-with-angular-material/src/app/app.module.ts
+++ typescript-with-angular-material/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutModule } from "@angular/cdk/layout";
 // import { MatToolbarModule } from '@angular/material/toolbar';
 // import { MatButtonModule } from '@angular/material/button';
 // import { MatSidenavModule } from '@angular/material/sidenav';
 // import { MatIconModule } from '@angular/material/icon';
 // import { MatListModule } from '@angular/material/list';
-import { HdComponent } from './hd/hd.component';
+import {AstroService} from "src/app/astro.service";
+import { HdComponent } from "./hd/hd.component";
 import { MaterialModule } from "./material.module";
-import { YaleComponent } from './yale/yale.component';
+import { YaleComponent } from "./yale/yale.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -25,9 +28,12 @@ import { YaleComponent } from './yale/yale.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MaterialModule
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AstroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
