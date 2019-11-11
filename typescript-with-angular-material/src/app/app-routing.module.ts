a287c5dbeb88b15e7a7697159dbd3162eb418197
Jason Ferguson <fergusonjason@gmail.com>
2019-11-11 12:23:32 -0500

    added route for ngc2000

--- typescript-with-angular-material/src/app/app-routing.module.ts
+++ typescript-with-angular-material/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HdComponent } from './hd/hd.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HdComponent } from "./hd/hd.component";
 import { YaleComponent } from "./yale/yale.component";
-import { YaleModule } from './yale/yale.module';
+import { YaleModule } from "./yale/yale.module";
+import { Ngc2000Component } from "./ngc2000/ngc2000.component";
 
 
 const routes: Routes = [
   {path: "", redirectTo: "/hd", pathMatch: "full" },
   {path: "hd", component: HdComponent},
-  {path: "yale", component: YaleComponent}
+  {path: "yale", component: YaleComponent},
+  {path: "ngc2000", component: Ngc2000Component}
 ];
 
 @NgModule({
