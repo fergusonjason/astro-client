bcaff1d381c338053f7017d9fbb8f2ff6bd46ac5
Jason Ferguson <fergusonjason@gmail.com>
2019-11-11 06:29:26 -0500

    basic implementation of data table

--- typescript-with-angular-material/src/app/yale/yale.component.ts
+++ typescript-with-angular-material/src/app/yale/yale.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AstroService } from "../astro.service";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { IQuery } from "../model/Query";
+import { Yale } from './yale';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
-  selector: 'app-yale',
-  templateUrl: './yale.component.html',
-  styleUrls: ['./yale.component.css']
+  selector: "app-yale",
+  templateUrl: "./yale.component.html",
+  styleUrls: ["./yale.component.css"],
+  providers: [AstroService ]
 })
 export class YaleComponent implements OnInit {
 
-  constructor() { }
+  queryForm: FormGroup;
+
+  queryObj: IQuery = { offset: 0, limit: 20, filter: {}};
+
+  private formGroupTemplate: object = {
+    queryField: "ID",
+    queryOp: "$eq",
+    queryParam: "",
+    sortField: "ID",
+    sortDir: 1,
+    offset: 0,
+    limit: 20
+  };
+
+  displayedItems: Yale[] = [];
+
+  dataSource: MatTableDataSource<Yale>;
+
+  displayColumns: string[] = ["ID", "name", "VisualMagnitude", "BV", "SpectralType", "NoteFlag"];
+
+  constructor(private astroService: AstroService, private formBuilder: FormBuilder) {
+    this.formBuilder.group(this.formGroupTemplate);
+   }
 
   ngOnInit() {
+    this.astroService.getPage<Yale>("yale", 0, 20).subscribe((data) => {
+      this.displayedItems = data.result;
+      this.dataSource = new MatTableDataSource<Yale>(this.displayedItems);
+    });
   }
 
 }
---
+++ typescript-with-angular-material/src/app/yale/yale.ts
@@ -0,0 +1,9 @@
+
+export interface Yale {
+    ID: number;
+    name: string | null;
+    VisualMagnitude: number;
+    BV: number;
+    NoteFlag: string | null;
+    xref: any;
+}
