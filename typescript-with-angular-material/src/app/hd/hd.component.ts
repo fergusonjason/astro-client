f2f6092620107fafe9939b07e9f2ceb17e5c1479
Jason Ferguson <fergusonjason@gmail.com>
2019-11-11 06:28:45 -0500

    basic implementation of data table

--- typescript-with-angular-material/src/app/hd/hd.component.ts
+++ typescript-with-angular-material/src/app/hd/hd.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { IQuery } from "../model/Query";
+import { HD } from "./hd";
+import { AstroService } from "../astro.service";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
-  selector: 'app-hd',
-  templateUrl: './hd.component.html',
-  styleUrls: ['./hd.component.css']
+  selector: "app-hd",
+  templateUrl: "./hd.component.html",
+  styleUrls: ["./hd.component.css"],
+  providers: [AstroService]
 })
 export class HdComponent implements OnInit {
 
-  constructor() { }
+  queryForm: FormGroup;
+
+  queryObj: IQuery = { offset: 0, limit: 20, filter: {}};
+
+  private formGroupTemplate: object = {
+    queryField: "ID",
+    queryOp: "$eq",
+    queryParam: "",
+    sortField: "ID",
+    sortDir: 1,
+    offset: 0,
+    limit: 20
+  };
+
+  displayedItems: HD[] = [];
+
+  dataSource: MatTableDataSource<HD>;
+
+  displayColumns: string[] = ["ID", "Ptm", "Ptg", "Intensity", "SpectralType", "Rem"];
+
+  constructor(private astroService: AstroService, private formBuilder: FormBuilder) {
+    this.formBuilder.group(this.formGroupTemplate);
+  }
 
   ngOnInit() {
+    this.astroService.getPage<HD>("hd", 0, 20).subscribe((data) => {
+      this.displayedItems = data.result;
+      this.dataSource = new MatTableDataSource<HD>(this.displayedItems);
+    });
+
   }
 
 }
---
+++ typescript-with-angular-material/src/app/hd/hd.ts
@@ -0,0 +1,11 @@
+export class HD {
+
+    id: number;
+    DM: string;
+    Ptm: number;
+    q_Ptg: number;
+    Rem: string | null;
+    SpectralType: string;
+    intensity: number;
+
+}
